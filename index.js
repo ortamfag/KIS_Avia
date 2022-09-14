5c407ff42b64a51af6130ac8f9b18c2c970b24a1
ortamfag <a_zhidov@outlook.com>
2022-09-14 18:33:57 +0300

    ADD: model user

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mysql = require('mysql')
+const authRouter = require('./Routes/authRouter')
 const PORT = process.env.PORT || 5000
 
 const connection = mysql.createConnection({
-    host     : 'phpmyadmin.fit.mospolytech.ru',
+    host     : 'std-mysql.ist.mospolytech.ru',
+    port     : '3306',
     user     : 'std_1408_kis',
     password : '12345678',
     database : 'std_1408_kis'
@@ -19,10 +21,12 @@ connection.connect(err => {
     }
 })
 
-
 const app = express()
 
-const start = () => {
+app.use(express.json())
+app.use("/auth", authRouter)
+
+const start = async () => {
     try {
         app.listen(PORT, () => console.log(`server started on port ${PORT}`))
     } catch (e) {
