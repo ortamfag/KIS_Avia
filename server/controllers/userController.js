6695cdc028641be4e3cb8ae43a72f79b3bd3ea87
ortamfag <a_zhidov@outlook.com>
2022-09-24 16:21:46 +0300

    ADD: form AddUser

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,7 +17,7 @@ exports.view = (req, res) => {
         console.log('Connected as ID' + connection.threadId)
 
         // User the connection
-        connection.query('SELECT * FROM users', (err, rows) => {
+        connection.query('SELECT * FROM users WHERE Active = "1"', (err, rows) => {
             //when done with connection, release it
             connection.release();
 
@@ -48,6 +48,38 @@ exports.find = (req, res) => {
 
             if (!err) {
                 res.render('home', { rows })
+            } else {
+                console.log(err);
+            }
+
+            console.log('The data from use table: \n', rows)
+        })
+    })
+}
+
+//Render the addUser form
+exports.form = (req, res) => {
+    res.render('add-user')
+}
+
+// Add User
+exports.create = (req, res) => {
+    const {first_name, last_name, email, password} = req.body
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err; //not connected
+        console.log('Connected as ID' + connection.threadId)
+
+        let searchTerm = req.body.search;
+        console.log(searchTerm)
+
+        // User the connection
+        connection.query('INSERT INTO users SET ID = ?, RoleID = ?, FirstName = ?, LastName = ?, Email = ?, Password = ?, Active = ?', [17, 1, first_name, last_name, email, password, 1], (err, rows) => {
+            //when done with connection, release it
+            connection.release();
+
+            if (!err) {
+                res.render('add-user')
             } else {
                 console.log(err);
             }
