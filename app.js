87e9e87114d784f5b320194b0cb73ab85682a2c1
ortamfag <a_zhidov@outlook.com>
2022-10-07 16:25:52 +0300

    ADD: start reg

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const PORT = process.env.PORT || 5000
 
-const authRouter = require('./authRouter')
+require('dotenv').config()
 const app = express()
 
-require('dotenv').config()
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(express.json())
-app.use('/auth', authRouter)
+// Parse application/json
+app.use(bodyParser.json())
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+app.engine('hbs', exphbs.engine( {extname: '.hbs'} ))
+app.set('view engine', 'hbs')
 
 // Connection Pool
 const pool = mysql.createPool({
@@ -21,11 +32,13 @@ const pool = mysql.createPool({
 })
 
 // Connect to DB
-
 pool.getConnection((err, connection) => {
     if(err) throw err; //not connected
     console.log('Connected as ID' + connection.threadId)
 })
+
+const routes = require('./server/routes/authRouter')
+app.use('/', routes);
 
 
 const start = () => {
---
+++ server/controllers/authController.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql')
+
+// Connection Pool
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+exports.regView = (req, res) => {
+    res.render('reg-user')
+}
+
+// Reg User
+exports.registration = (req, res) => {
+    const {email, password} = req.body
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err; //not connected
+        console.log('Connected as ID' + connection.threadId)
+
+        let searchTerm = req.body.search;
+        console.log(searchTerm)
+
+        // User the connection
+        connection.query('INSERT INTO users SET ID = ?, RoleID = ?, Email = ?, Password = ?, Active = ?', [18, 1, email, password, 1], (err, rows) => {
+            //when done with connection, release it
+            connection.release();
+
+            if (!err) {
+                res.render('reg-user', { alert: "User added successfully" })
+            } else {
+                console.log(err);
+            }
+
+            console.log('The data from use table: \n', rows)
+        })
+    })
+}
