14b681315ebe1eda0ed8d994ac4cf410d4f2c659
ortamfag <a_zhidov@outlook.com>
2022-11-09 17:04:58 +0300

    ADD: spend time

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -52,33 +52,68 @@ exports.login = (req, res) => {
                     res.cookie('token', token)     
                     res.clearCookie('badLogin')
 
+                    //находим дату
+                    let thisDate = new Date();
+                    thisDate = thisDate.toLocaleString()
+                    thisDate = thisDate.split(',')
+                    let dateOfLogin = thisDate[0]
+                    let timeOfLogin = thisDate[1].split(' ').join('')
+                    
+
+                    res.cookie('loginDate', dateOfLogin) //если это остаётся при некст логине, то вывести попап
+                    res.cookie('loginTime', timeOfLogin)
+
+                    res.cookie('email', email)
+
+                    //находим почту
+                    let nameOfNewTable = email.split('@')
+                    nameOfNewTable = nameOfNewTable[0]
+
+                    let changeWrongSymbols = nameOfNewTable.split('')      
+                    for (let i = 0; i <= changeWrongSymbols.length; i++) {
+                        if ((changeWrongSymbols[i] === '.') || (changeWrongSymbols[i] === '-')) {
+                            changeWrongSymbols[i] = '_'
+                        }
+                    }
+
+                    nameOfNewTable = changeWrongSymbols.join('')
+                    res.cookie('nameOfNewTable', nameOfNewTable)
+
                     pool.getConnection((err, connection) => {
                         if (err) throw err; //not connected
                         console.log('Connected as ID' + connection.threadId)
-
-                        let nameOfNewTable = email.split('@')
-                        nameOfNewTable = nameOfNewTable[0]
-
-                        let changeWrongSymbols = nameOfNewTable.split('')      
-                        for (let i = 0; i <= changeWrongSymbols.length; i++) {
-                            if ((changeWrongSymbols[i] === '.') || (changeWrongSymbols[i] === '-')) {
-                                changeWrongSymbols[i] = '_'
-                            }
-                        }
-
-                        nameOfNewTable = changeWrongSymbols.join('')
-
-                        
                 
-                        connection.query(`INSERT INTO ${nameOfNewTable} SET Active = ?', [1]`, (err, rows) => {
+                        connection.query(`SHOW TABLES LIKE "${nameOfNewTable}"`, (err, result) => {
                             connection.release();
                 
                             if (!err) {
-                                if (candidate[0].RoleID === 1) {
-                                    res.redirect('homeAdmin')
+                                if (result.length != 0) {
+                                    if (candidate[0].RoleID === 1) {
+                                        res.redirect('homeAdmin')
+                                    } else {
+                                        res.redirect('homeUser')
+                                    }
                                 } else {
-                                    res.redirect('homeUser')
+                                    pool.getConnection((err, connection) => {
+                                        if (err) throw err; //not connected
+                                        console.log('Connected as ID' + connection.threadId)
+                                
+                                        connection.query(`CREATE TABLE ${nameOfNewTable} (Name VARCHAR(150), Date VARCHAR(150), LogTime VARCHAR(150), LogOutTime VARCHAR(150), TimeSpendOne INT(20), Reason VARCHAR(150), TimeSpend INT(20), Crashes INT(10))`, (err, rows) => {
+                                            connection.release();
+                                
+                                            if (!err) { 
+                                                if (candidate[0].RoleID === 1) {
+                                                    res.redirect('homeAdmin')
+                                                } else {
+                                                    res.redirect('homeUser')
+                                                }
+                                            } else {
+                                                console.log(err);
+                                            }
+                                        })
+                                    })
                                 }
+                                
                             } else {
                                 console.log(err);
                             }
@@ -108,5 +143,29 @@ exports.login = (req, res) => {
 // Login User
 exports.exit = (req, res) => {
     res.clearCookie('token')
-    res.render('exit-page', {alert: 'Вы успешно вышли из аккаунта'})
+    pool.getConnection((err, connection) => {
+        if (err) throw err; //not connected
+        console.log('Connected as ID' + connection.threadId)
+        console.log(req.cookies.email)
+        console.log(req.cookies.loginDate)
+        console.log(req.cookies.loginTime)
+        console.log(req.cookies.nameOfNewTable)
+
+        //находим дату
+        let thisLogOutDate = new Date();
+        thisLogOutDate = thisLogOutDate.toLocaleString()
+        thisLogOutDate = thisLogOutDate.split(',')
+        let dateOfLoginOut = thisLogOutDate[0]
+        let timeOfLoginOut = thisLogOutDate[1].split(' ').join('')
+
+        connection.query(`INSERT INTO ${req.cookies.nameOfNewTable} SET Name = ?, Date = ?, LogTime = ?, LogOutTime = ?, TimeSpendOne = ?, Reason = ?, TimeSpend = ?, Crashes = ?`, [req.cookies.email, req.cookies.loginDate, req.cookies.loginTime, timeOfLoginOut, 0, 'test', 0, 0], (err, rows) => {
+            connection.release();
+
+            if (!err) {
+                res.render('exit-page', {alert: 'Вы успешно вышли из аккаунта'})
+            } else {
+                console.log(err);
+            }
+        })
+    })
 }
