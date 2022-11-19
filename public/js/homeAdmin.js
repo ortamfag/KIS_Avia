5ecf9d1fd2b3dedb8555e29afdabaa73289235c4
Artemy Zhidov <71493037+ortamfag@users.noreply.github.com>
2022-11-19 18:02:15 +0300

    ADD: offices dropdown

--- public/js/homeAdmin.js
+++ public/js/homeAdmin.js
@@ -12,3 +12,65 @@ activeUser.forEach(item => {
         item.parentNode.style.backgroundColor = '#E9D3D4'
     }
 })
+
+const officesDropdown = document.getElementById('officesDropdown')
+
+function checkOffice() {
+    let officesDropdownValue = officesDropdown.value 
+    const allOffice = document.querySelectorAll('#offices')
+    console.log(officesDropdownValue)
+
+    for (let i = 0; i < allOffice.length; i++) {
+        allOffice[i].parentNode.style.display = 'table-row'
+    }
+
+    switch(officesDropdownValue) {
+        case 'All offices':
+            for (let i = 0; i < allOffice.length; i++) {
+                allOffice[i].parentNode.style.display = 'table-row'
+            }
+            break
+
+        case 'Abu dhabi':
+            for (let i = 0; i < allOffice.length; i++) {
+                if (allOffice[i].innerHTML != officesDropdownValue) {
+                    allOffice[i].parentNode.style.display = 'none'
+                }
+            }
+            break
+
+        case 'Cairo':
+            for (let i = 0; i < allOffice.length; i++) {
+                if (allOffice[i].innerHTML != officesDropdownValue) {
+                    allOffice[i].parentNode.style.display = 'none'
+                }
+            }
+            break
+
+        case 'Bahrain':
+            for (let i = 0; i < allOffice.length; i++) {
+                if (allOffice[i].innerHTML != officesDropdownValue) {
+                    allOffice[i].parentNode.style.display = 'none'
+                }
+            }
+            break
+            
+        case 'Doha':
+            for (let i = 0; i < allOffice.length; i++) {
+                if (allOffice[i].innerHTML != officesDropdownValue) {
+                    allOffice[i].parentNode.style.display = 'none'
+                }
+            }
+            break 
+            
+        case 'Riyadh':
+            for (let i = 0; i < allOffice.length; i++) {
+                if (allOffice[i].innerHTML != officesDropdownValue) {
+                    allOffice[i].parentNode.style.display = 'none'
+                }
+            }
+            break    
+
+        
+    }
+} 
